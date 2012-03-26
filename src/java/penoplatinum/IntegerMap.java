17c47661e3a3e1b19e102e72e2779f094cf86bac
rupsbant <rubenlapauw@hotmail.com>
2012-03-26 13:51:41 +0200

    IntegerMap debugging

--- src/java/penoplatinum/IntegerMap.java
+++ src/java/penoplatinum/IntegerMap.java
@@ -4,7 +4,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class IntegerMap<Value>{
+public class IntegerMap<Value> {
+
   private int size = 0;
   private int[] keys = new int[10];
   private Value[] values = (Value[]) new Object[10];
@@ -14,12 +15,11 @@ public class IntegerMap<Value>{
     if (index == -1) {
       return null;
     }
-
     return values[index];
   }
 
   private int indexOf(int key) {
-    for (int i = 0; i < keys.length; i++) {
+    for (int i = 0; i < size; i++) {
       if (keys[i] == key) {
         return i;
       }
@@ -31,7 +31,7 @@ public class IntegerMap<Value>{
     int index = indexOf(key);
     if (index == -1) {
       ensureCapacity();
-      keys[size] = index;
+      keys[size] = key;
       values[size] = value;
       size++;
       return;
@@ -49,25 +49,26 @@ public class IntegerMap<Value>{
 
   public List<Value> values() {
     List<Value> out = new ArrayList<Value>(size);
-    for(Value v : values){
-      out.add(v);
+    for (int i = 0; i < size; i++) {
+      out.add(values[i]);
     }
     return out;
   }
 
   public int findKey(Value g) {
     for (int i = 0; i < size; i++) {
-      if (g.equals(values[i]))
+      if (g.equals(values[i])) {
         return keys[i];
+      }
     }
     return -1;
   }
-  
-  private void ensureCapacity(){
-    if(size < keys.length){
+
+  private void ensureCapacity() {
+    if (size < keys.length) {
       return;
     }
-    keys = Arrays.copyOf(keys, keys.length*3/2);
+    keys = Arrays.copyOf(keys, keys.length * 3 / 2);
     values = Arrays.copyOf(values, keys.length);
   }
 }
